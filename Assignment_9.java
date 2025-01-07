1e2aaf90d53f1c4fd9f298578223e5028b99185f
Riya6567 <deyr13733@gmail.com>
2025-01-07 10:05:43 +0530

    Add files via upload

---
+++ Assignment_9.java
@@ -0,0 +1,25 @@
+public class Assignment_9 implements Runnable // Real class name - Main
+{
+    public void run() {
+        try {
+            for(int i=0; i<5; i++)
+            {
+                System.out.println("Thread:" +Thread.currentThread().getName()+" is running");
+                Thread.sleep(500);
+            }
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Thread:" + Thread.currentThread().getName()+ " interupted." );
+        }
+    }
+    public static void main(String arr[])
+    {
+        Assignment_9 thread1 = new Assignment_9();
+        Assignment_9 thread2 = new Assignment_9();
+        Thread t1 = new Thread (thread1, "thread-1");
+        Thread t2 = new Thread (thread2, "Thread-2");
+        t1.start();
+        t2.start();
+    }
+}
